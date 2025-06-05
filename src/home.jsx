0706ed92435eca1bdbb03f4b560e80ed33d5e570
AnnaSkudsveen <annaskudsveen@hotmail.com>
2025-06-05 22:47:08 +0200

    feat: english translation

--- src/home.jsx
+++ src/home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import baatkos from "../images/Båtkos.jpeg";
 import flaggBaat from "../images/flagg-båt.jpeg";
@@ -11,22 +11,38 @@ import Baat from "../images/ac829dde-4a6b-46da-a757-7d606e39c7e7.jpeg";
 import video from "../images/235b7704-0543-4fa3-bdda-760510b5ae20.mp4";
 
 function Homepage() {
+  const [language, setLanguage] = useState("no");
+
+  const toggleLanguage = () => {
+    setLanguage(language === "NO" ? "EN" : "NO");
+  };
+
   return (
     <>
       <header className="bg-[#115488] p-10 flex flex-col items-center justify-center gap-14">
+        <div className="w-full flex justify-end">
+          <button
+            onClick={toggleLanguage}
+            className="bg-[#115488] text-white py-2 px-4 rounded-lg border border-white"
+          >
+            {language === "NO" ? "English" : "Norwegian"}
+          </button>
+        </div>
         <img
           src={jsArcticSailing}
           alt=""
           className="w-[300px] sm:w-[400] md:w-[450px]"
         />
         <h1 className=" text-white font-extrabold text-3xl tracking-wide text-center">
-          Velkommen til JS Arctic Sailing
+          {language === "NO"
+            ? "Velkommen til JS Arctic Sailing"
+            : "Welcome to JS Arctic Sailing"}
         </h1>
       </header>
 
       <main className="flex flex-col items-center justify-center mt-10 gap-10">
         <h2 className=" text-[#115488] text-3xl font-extrabold">
-          Hva vi tilbyr
+          {language === "NO" ? "Hva vi tilbyr" : "What we offer"}
         </h2>
 
         <section className="flex flex-col lg:flex-row items-center lg:items-start justify-center gap-10 lg:gap-10 lg:mx-20 w-full">
@@ -37,24 +53,47 @@ function Homepage() {
               alt=""
             />
 
-            <h3 className="text-2xl font-bold]">Kurs vi tilbyr</h3>
+            <h3 className="text-2xl font-bold]">
+              {language === "NO" ? "Kurs vi tilbyr" : "Courses we offer"}
+            </h3>
 
             <div className="flex justify-center">
               <ul className="list-disc pl-5 text-gray-700 flex flex-col gap-1 text-start">
-                <li>ICC - Internasjonalt båtsertifikat</li>
-                <li>Kjøp og salg av båt</li>
-                <li>Transport av båt sjøveien (Maks lengde 24m/75 fot)</li>
-                <li>Crew tjenester</li>
-                <li>Verdivurdering av båt</li>
                 <li>
-                  Krankurs (Elektrisk søylekran for båtopptak) I samarbeid med
-                  Safe Opplæring
+                  {language === "NO"
+                    ? "ICC - Internasjonalt båtsertifikat"
+                    : "ICC - International Boat Certificate"}
+                </li>
+                <li>
+                  {language === "NO"
+                    ? "Kjøp og salg av båt"
+                    : "Boat purchase and sale"}
+                </li>
+                <li>
+                  {language === "NO"
+                    ? "Transport av båt sjøveien (Maks lengde 24m/75 fot)"
+                    : "Boat transport by sea (Max length 24m/75 feet)"}
+                </li>
+                <li>
+                  {language === "NO" ? "Crew tjenester" : "Crew services"}
+                </li>
+                <li>
+                  {language === "NO"
+                    ? "Verdivurdering av båt"
+                    : "Boat valuation"}
+                </li>
+                <li>
+                  {language === "NO"
+                    ? "Krankurs (Elektrisk søylekran for båtopptak) I samarbeid med Safe Opplæring"
+                    : "Crane course (Electric pillar crane for boat lifting) In collaboration with Safe Opplæring"}
                 </li>
               </ul>
             </div>
 
             <p className="text-gray-700">
-              Vi har samarbeid med Maritim Facilitator Norway AS
+              {language === "NO"
+                ? "Vi har samarbeid med Maritim Facilitator Norway AS"
+                : "We collaborate with Maritime Facilitator Norway AS"}
             </p>
           </div>
 
@@ -69,20 +108,15 @@ function Homepage() {
 
             <div className="text-gray-700 flex flex-col items-center justify-center gap-5 text-start">
               <p>
-                Jeg er født i 1961 og har tilbrakt 40 år som pilot, en karriere
-                som har gitt meg stor kunnskap om navigasjon, presisjon og
-                tekniske systemer. Jeg har alltid hatt en sterk interesse for
-                det maritime. Gjennom årene har jeg eid flere seil- og
-                motorbåter, og har lang erfaring med kjøp, restaurering og salg
-                av båter.
+                {language === "NO"
+                  ? "Jeg er født i 1961 og har tilbrakt 40 år som pilot, en karriere som har gitt meg stor kunnskap om navigasjon, presisjon og tekniske systemer. Jeg har alltid hatt en sterk interesse for det maritime. Gjennom årene har jeg eid flere seil- og motorbåter, og har lang erfaring med kjøp, restaurering og salg av båter."
+                  : "I was born in 1961 and have spent 40 years as a pilot, a career that has given me extensive knowledge of navigation, precision, and technical systems. I have always had a strong interest in maritime matters. Over the years, I have owned several sailboats and motorboats, and have extensive experience with buying, restoring, and selling boats."}
               </p>
 
               <p>
-                Som både flyinstruktør og maritim instruktør innen navigasjon,
-                meteorologi og motorlære har jeg bred kompetanse på tvers av
-                luftfart og sjøfart. I tillegg har jeg solid erfaring med
-                verdivurdering og tilstandsrapporter av båter, basert på både
-                teknisk innsikt og praktisk erfaring.
+                {language === "NO"
+                  ? "Som både flyinstruktør og maritim instruktør innen navigasjon, meteorologi og motorlære har jeg bred kompetanse på tvers av luftfart og sjøfart. I tillegg har jeg solid erfaring med verdivurdering og tilstandsrapporter av båter, basert på både teknisk innsikt og praktisk erfaring."
+                  : "As both a flight instructor and maritime instructor in navigation, meteorology, and engine theory, I have broad expertise across both aviation and maritime fields. Additionally, I have solid experience with boat valuations and condition reports, based on both technical insight and practical experience."}
               </p>
             </div>
           </div>
@@ -93,14 +127,21 @@ function Homepage() {
               className="h-[320px] md:h-[450px] w-[320px] md:w-[450px] lg:w-[320px] lg:h-[320px] object-cover rounded-full"
               alt=""
             />
-            <h2 className="text-2xl font-bold] text-center">Andre tjenester</h2>
+            <h2 className="text-2xl font-bold] text-center">
+              {" "}
+              {language === "NO" ? "Andre tjenester" : "Other services"}
+            </h2>
 
             <div className="w-5/6 md:w-[450px]">
               <ul className="list-disc pl-5 text-gray-700 flex flex-col gap-1 text-start">
-                <li>Navigasjon</li>
-                <li>Meteorologi</li>
-                <li>Nattsailas</li>
-                <li>Maritime lover og regler</li>
+                <li> {language === "NO" ? "Navigasjon" : "Navigation"}</li>
+                <li>{language === "NO" ? "Meteorologi" : "Meteorology"}</li>
+                <li>{language === "NO" ? "Nattsailas" : "Night Sailing"}</li>
+                <li>
+                  {language === "NO"
+                    ? "Maritime lover og regler"
+                    : "Maritime laws and regulations"}
+                </li>
               </ul>
             </div>
           </div>
@@ -127,20 +168,37 @@ function Homepage() {
 
         <div>
           <h2 className=" text-[#115488] text-3xl font-extrabold text-center">
-            Kontakt oss
+            {language === "NO" ? "Kontakt oss" : "Contact Us"}
           </h2>
 
           <div className="flex flex-col gap-10 mt-10">
             <h3 className="text-2xl font-bold] ">JS Arctic Sailing</h3>
 
-            <p>Vi i Jan Stalsberg Arctic Sailing selger tjenester og kurs.</p>
+            <p>
+              {" "}
+              {language === "NO"
+                ? "Vi i Jan Stalsberg Arctic Sailing selger tjenester og kurs."
+                : "We at Jan Stalsberg Arctic Sailing offer services and courses."}
+            </p>
 
-            <p>For prisanslag - ta kontakt på telefon eller via mail.</p>
+            <p>
+              {language === "NO"
+                ? "For prisanslag - ta kontakt på telefon eller via mail."
+                : "For price quotes - contact us by phone or email."}
+            </p>
 
             <div className="flex flex-col justify-center gap-4 text-gray-700">
               <p>Knarbergveien 1a, 3133 Duken, Norway</p>
-              <p>Epost: jan.stalsberg@gmail.com</p>
-              <p>Telefon: +47 909 11 251</p>
+              <p>
+                {language === "NO"
+                  ? "Epost: jan.stalsberg@gmail.com"
+                  : "Email: jan.stalsberg@gmail.com"}
+              </p>
+              <p>
+                {language === "NO"
+                  ? "Telefon: +47 909 11 251"
+                  : "Phone: +47 909 11 251"}
+              </p>
             </div>
           </div>
         </div>
